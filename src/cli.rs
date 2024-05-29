1814c1d71a77249a8a8c66ac4b6aff2009c6d116
dablenparty <hdavenport8902@gmail.com>
2024-05-29 13:08:53 -0400

    Add subcommands

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,25 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-/// The command-line interface for the application.
+/// This is a simple CLI tool to manage a list of todos.
+/// Running with no arguments will run in interactive mode.
+/// Subcommands are provided for quick access to specific actions.
 #[derive(Debug, Parser)]
-pub struct CliArgs {}
+#[command(version, about, author, long_about = None)]
+pub struct CliArgs {
+    #[clap(subcommand)]
+    pub subcommand: Option<CliCommand>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum CliCommand {
+    /// Add a new todo to the list
+    Add,
+    /// Edit an existing todo
+    Edit,
+    /// Remove a todo from the list
+    #[command(visible_alias = "rm")]
+    Remove,
+    /// List all todos or apply filters
+    #[command(visible_alias = "ls")]
+    List,
+}
