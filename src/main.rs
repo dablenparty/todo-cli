5dda40bc706e0e1e3b267f44eeaeb8f987662203
dablenparty <hdavenport8902@gmail.com>
2024-05-29 15:21:31 -0400

    Prompt for command if not provided

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 use clap::Parser;
+use inquire::Select;
 use serde::{Deserialize, Serialize};
+use strum::VariantArray;
 
 mod cli;
 
@@ -11,13 +15,17 @@ struct TodoItem {
     // TODO: due date?
 }
 
-fn main() {
-    let cli = cli::CliArgs::parse();
+fn main() -> anyhow::Result<()> {
+    let cli = cli::Args::parse();
     println!("{cli:#?}");
 
-    if let Some(cmd) = cli.subcommand {
-        cmd.handle_command();
-    }
+    let command = if let Some(cmd) = cli.subcommand {
+        cmd
+    } else {
+        Select::new("Select an option:", cli::Command::VARIANTS.to_vec()).prompt()?
+    };
 
-    todo!("Handle interactive mode")
+    command.handle_command();
+
+    Ok(())
 }
