809444172ebe24643cd2fc9ec5cc42d55fa46f3e
dablenparty <hdavenport8902@gmail.com>
2024-05-29 15:01:52 -0400

    Create TodoItem struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use clap::Parser;
+use serde::{Deserialize, Serialize};
 
 mod cli;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+struct TodoItem {
+    pub short_desc: String,
+    pub long_desc: Option<String>,
+    pub completed: bool,
+    // TODO: due date?
+}
 
 fn main() {
     let cli = cli::CliArgs::parse();
