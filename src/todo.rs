43309e27457988e64af2bdcea8e781bdc348f292
dablenparty <hdavenport8902@gmail.com>
2024-05-29 15:34:03 -0400

    Create read & write functions

--- src/todo.rs
+++ src/todo.rs
@@ -1,5 +1,8 @@
+use std::env::current_dir as current_working_dir;
+
 use serde::{Deserialize, Serialize};
 
+pub const TODO_FILE_NAME: &str = ".todos";
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(clippy::module_name_repetitions)]
@@ -8,4 +11,32 @@ pub struct TodoItem {
     pub long_desc: Option<String>,
     pub completed: bool,
     // TODO: due date? uuid?
+}
+
+/// Read the todo file from the current directory. If the file does not exist, an empty Vec is returned.
+///
+/// # Errors
+///
+/// This function will return an error if the file cannot be read or if the file is not valid RON.
+pub fn read_todo_file() -> anyhow::Result<Vec<TodoItem>> {
+    let todo_path = current_working_dir()?.join(TODO_FILE_NAME);
+    if !todo_path.try_exists().unwrap_or_default() {
+        return Ok(Vec::new());
+    }
+    let todo_str = std::fs::read_to_string(todo_path)?;
+    let todos: Vec<TodoItem> = ron::from_str(&todo_str)?;
+    Ok(todos)
+}
+
+/// Write the todo file to the current directory. This will overwrite the existing file.
+/// If the file does not exist, it will be created.
+///
+/// # Errors
+///
+/// This function will return an error if the file cannot be written or if the todos cannot be serialized to RON.
+pub fn write_todo_file(todos: &[TodoItem]) -> anyhow::Result<()> {
+    let base_dir = current_working_dir()?;
+    let todo_str = ron::to_string(todos)?;
+    std::fs::write(base_dir.join(TODO_FILE_NAME), todo_str)?;
+    Ok(())
 }
