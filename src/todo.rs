5dcc3747d165ec25bdda1949cbc8d672ce1cd104
dablenparty <hdavenport8902@gmail.com>
2024-05-29 23:02:37 -0400

    Remove completed todo

--- src/todo.rs
+++ src/todo.rs
@@ -12,7 +12,7 @@ pub struct TodoItem {
     pub short_desc: String,
     pub long_desc: Option<String>,
     pub completed: bool,
-    // TODO: due date? uuid?
+    // TODO: due date?
 }
 
 impl std::fmt::Display for TodoItem {
